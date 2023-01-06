e9a46d1778e524d8b69d60967792dc74f3cbdd6d
Dmitry Melentev <dmitry.tomahawk@yandex.ru>
2023-01-07 03:50:31 +0500

    feat(client): :lips: add font

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,6 +1,36 @@
-import '../styles/globals.css';
+import '../styles/global.css';
 import type { AppProps } from 'next/app';
+import localFont from '@next/font/local';
+
+const ggSans = localFont({
+  src: [
+    {
+      path: '../assets/fonts/ggsans/ggsans-Normal.woff2',
+      weight: '400',
+    },
+    {
+      path: '../assets/fonts/ggsans/ggsans-Medium.woff2',
+      weight: '500',
+    },
+    {
+      path: '../assets/fonts/ggsans/ggsans-Semibold.woff2',
+      weight: '600',
+    },
+    {
+      path: '../assets/fonts/ggsans/ggsans-Bold.woff2',
+      weight: '700',
+    },
+    {
+      path: '../assets/fonts/ggsans/ggsans-ExtraBold.woff2',
+      weight: '800',
+    },
+  ],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={ggSans.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
