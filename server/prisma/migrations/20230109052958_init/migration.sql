201d4b7d00e71f6dcde70d65b60f9147a5bfce99
Melentev Dmitry <78354890+melloc1801@users.noreply.github.com>
2023-01-09 10:41:54 +0500

    Feature 6 initialize prisma (#39)
    * feat(server): :package: add prisma packages
    
    * feat(server): :necktie: initialize prisma
    
    * feat(server): :necktie: initialize db schema
    
    * feat(server): :wrench: add db url template

---
+++ server/prisma/migrations/20230109052958_init/migration.sql
@@ -0,0 +1,108 @@
+-- CreateEnum
+CREATE TYPE "UserStatus" AS ENUM ('ONLINE', 'IDLE', 'DND', 'OFFLINE');
+
+-- CreateEnum
+CREATE TYPE "FriendshipStatusEnum" AS ENUM ('REQUESTED', 'ACCEPTED', 'DECLINED', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "ChatTypeEnum" AS ENUM ('DIRECT', 'GROUP', 'SERVER');
+
+-- CreateEnum
+CREATE TYPE "ChatRoleEnum" AS ENUM ('MEMBER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "outerId" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "isConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "onlineStatus" "UserStatus" NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Friendship" (
+    "requesterId" INTEGER NOT NULL,
+    "addresseeId" INTEGER NOT NULL,
+    "status" "FriendshipStatusEnum" NOT NULL,
+
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("requesterId","addresseeId")
+);
+
+-- CreateTable
+CREATE TABLE "Chat" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "avatarUrl" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+    "type" "ChatTypeEnum" NOT NULL,
+
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Member" (
+    "userId" INTEGER NOT NULL,
+    "chatId" INTEGER NOT NULL,
+
+    CONSTRAINT "Member_pkey" PRIMARY KEY ("userId","chatId")
+);
+
+-- CreateTable
+CREATE TABLE "TextChannel" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "chatId" INTEGER NOT NULL,
+
+    CONSTRAINT "TextChannel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL,
+    "isUpdated" BOOLEAN NOT NULL DEFAULT false,
+    "userId" INTEGER NOT NULL,
+    "textChannelId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_outerId_key" ON "User"("username", "outerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Chat_name_key" ON "Chat"("name");
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_requesterId_fkey" FOREIGN KEY ("requesterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_addresseeId_fkey" FOREIGN KEY ("addresseeId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Member" ADD CONSTRAINT "Member_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "TextChannel" ADD CONSTRAINT "TextChannel_chatId_fkey" FOREIGN KEY ("chatId") REFERENCES "Chat"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_textChannelId_fkey" FOREIGN KEY ("textChannelId") REFERENCES "TextChannel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly _prismaService: PrismaService) {}
+
   getHello(): string {
     return 'Hello World!';
   }
